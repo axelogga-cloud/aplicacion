917e7fce39c9d8e0dfa89d95470a8b4ff252f3ae
pl014 <pl014@LAPTOP-QMBB8PKR>
2026-06-04 08:56:01 -0600

    Agregar archivos de proyecto.

---
+++ aplicacion/Controlers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using aplicacion.Models;
+using aplicacion.Services;
+
+namespace app_to_do.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly AuthService _authService;
+
+        public AccountController(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LogingViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            bool valido =
+                _authService.ValidarUsuario(
+                    model.Email,
+                    model.Password);
+
+            if (valido)
+                return RedirectToAction("Welcome");
+
+            return RedirectToAction("Error");
+        }
+
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
